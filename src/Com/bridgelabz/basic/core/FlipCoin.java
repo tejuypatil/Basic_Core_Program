3dadb4b51ef3d7e3b78b7c3e6b51ee20a1bf6233
tejuypatil <tejuypatil@gmail.com>
2022-07-03 18:54:08 +0530

    Added Program of Flip coin

---
+++ src/Com/bridgelabz/basic/core/FlipCoin.java
@@ -0,0 +1,35 @@
+package Com.bridgelabz.basic.core;
+
+import static java.lang.Math.random;
+
+public class FlipCoin {
+    public static void main(String[] args) {
+        int heads = 0;
+        int tails = 0;
+        //double random =0.0;
+        //int count=1;
+        int NumberOfTrails = 4;
+        {
+            for (int i = 0; i < NumberOfTrails; i++) {
+
+                if (Math.random() < 0.5) {
+                    System.out.println("Head");
+                    heads++;
+                } else {
+                    System.out.println("Tail");
+                    tails++;
+                }
+            }
+        }
+
+        System.out.println("Number of count Head: " + heads);
+        System.out.println("Number of count Tails: " + tails);
+
+        double headpercent = (double) heads / NumberOfTrails * 100;
+        double tailpercent = (double) tails / NumberOfTrails * 100;
+
+        System.out.println("Percentage of Head: " + headpercent);
+
+        System.out.println("Percentage of Tails " + tailpercent);
+    }
+}
