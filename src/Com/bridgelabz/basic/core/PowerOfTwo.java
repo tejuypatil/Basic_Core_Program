2a81a5df8a38ec4ddd92e049748cae3e53f8cc42
tejuypatil <tejuypatil@gmail.com>
2022-07-04 10:01:03 +0530

    Added Power of two program

--- src/Com/bridgelabz/basic/core/PowerOfTwo.java
+++ src/Com/bridgelabz/basic/core/PowerOfTwo.java
@@ -1,4 +1,16 @@
 package Com.bridgelabz.basic.core;
 
+import java.util.Scanner;
+
 public class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the Power of Two:");
+        int N=scanner.nextInt();
+        for (int i = 0; i <N ; i++) {
+            System.out.println("2^"+i+"\t" +Math.pow(2,i));
+
+        }
+
+    }
 }
